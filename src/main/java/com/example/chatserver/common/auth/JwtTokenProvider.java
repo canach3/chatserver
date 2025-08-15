b82c8e739072d1d88217630a14f1bd557f2ee759
JungYoon <canach3@gmail.com>
2025-08-15 15:18:21 +0900

    feat: JWT 기반 로그인 방식 구현

--- src/main/java/com/example/chatserver/common/auth/JwtTokenProvider.java
+++ src/main/java/com/example/chatserver/common/auth/JwtTokenProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Date;
