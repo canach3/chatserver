be98fc5dc51a57a53b2331b9b7edfcd0eb7bbaa8
JungYoon <canach3@gmail.com>
2025-08-17 21:26:41 +0900

    feat: 채팅 관련 controller service, repository 생성

---
+++ src/main/java/com/example/chatserver/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,8 @@
+package com.example.chatserver.chat.repository;
+
+import com.example.chatserver.chat.domain.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+}
---
+++ src/main/java/com/example/chatserver/chat/repository/ReadStatusRepository.java
@@ -0,0 +1,8 @@
+package com.example.chatserver.chat.repository;
+
+import com.example.chatserver.chat.domain.ReadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadStatusRepository extends JpaRepository<ReadStatus, Long> {
+
+}
